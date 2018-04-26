000d0d2b0617cac5bb8f6f698eeb7d1f0e1a76ea
Rodiel Martinez <Rodielmj@gmail.com>
2018-04-25 22:40:39 -0400

    Challenge #11 In process

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,31 @@
+
+/* Get Our Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* Build out functions */
+
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+    // if (video.paused){
+    //     video.play();
+    // }else{
+    //     video.pause();
+    // }
+}
+function updateButton() {
+    const icon = this.pause?'►':'❚ ❚';
+    console.log('Update the button');
+}
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause',updateButton);
+
+toggle.addEventListener('click',togglePlay);
